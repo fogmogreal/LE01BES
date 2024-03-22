43e7b98597dc3fee146624723af540a33f09026d
n <narcizo.marquess@gmail.com>
2024-03-22 09:46:57 -0300

    main

--- Exemplo06.java
+++ Exemplo06.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Exemplo06 {
     
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Insira a temperatura: " );
+        int C = leitor.nextInt();
+        
+        System.out.println("A temperatura em Celsius é: " + C);
+        
+        int F = (9 * C + 160) / 5;
+        
+        System.out.println("A conversão da temperatura para Fahrenheit é: " + F);
+        
+        leitor.close();
+        
+    }
+
+
 }
--- Exemplo07.java
+++ Exemplo07.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Exemplo07 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+
+        int numero = leitor .nextInt();
+
+        leitor.close();
+    }
 }
