342420fdf34c1ea4d034578c664ad190eb20ce80
n <narcizo.marquess@gmail.com>
2024-03-24 22:09:03 -0300

    main

--- Exemplo05.java
+++ Exemplo05.java
@@ -1,3 +1,26 @@
-public class Exemplo05 {
+import java.util.Scanner;
 
+public class Exemplo05 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Insira o valor de A:");
+        int a = leitor.nextInt();
+        System.out.println("Agora, insira o valor de B:");
+        int b = leitor.nextInt();
+
+        System.out.println("Valores originais:");
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Valores trocados:");
+        System.out.println("A: " + a);
+        System.out.println("B: " + b);
+
+        leitor.close();
+    }
 }
--- Exemplo07.java
+++ Exemplo07.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class Exemplo07 {
-
     public static void main(String[] args) {
-
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um número:");
 
-        int numero = leitor .nextInt();
+        int numero = leitor.nextInt();
+
+        if (numero >= 100 && numero <= 200) {
+            System.out.println("O número está dentro do intervalo entre 100 e 200.");
+        } else {
+            System.out.println("O número não está dentro do intervalo entre 100 e 200.");
+        }
 
         leitor.close();
     }
